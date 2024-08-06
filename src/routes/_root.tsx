c632d3b4d0e28cc9e8d9ec8e3ca3a01b9f5165f8
Carlos Estrada <carlosestrada94@outlook.com>
2024-08-06 18:59:50 -0500

    tanstack router initial setup

---
+++ src/routes/_root.tsx
@@ -0,0 +1,13 @@
+import { Link, Outlet, createRootRoute } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+
+export const Route = createRootRoute({
+    component: () => (
+        <div>
+            <Link to={"/"}>Home</Link>
+            <Link to={"/about"}>About</Link>
+            <Outlet />
+            <TanStackRouterDevtools />
+        </div>
+    ),
+});
